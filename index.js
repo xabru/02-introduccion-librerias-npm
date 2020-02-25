eb0e3cff089730f847461b05f8dfd82b7936c537
xabru <xabru@yandex.com>
2020-02-24 22:08:47 -0800

    ADD Primer modulo simple creado

---
+++ index.js
@@ -0,0 +1,13 @@
+module.exports = {
+
+    hola: function() {
+        console.log('Hola mundo!');
+    },
+    holaPersonalizado: function(nombre) {
+        console.log(`Hola ${nombre}`);
+    },
+    holaAlCursoNPM: function() {
+        console.log(`Hola al curso NPM!`);
+    }
+
+}
